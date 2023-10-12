1cae684e10feddb5e63c50e4fddceb9e46f4e67a
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-12 18:30:43 -0400

    added v2 and switched from require to import x from y

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function isValidUsername(username) {
   return typeof username === 'string' && username.trim() !== '';
 }
 
-module.exports = async function getPinnedRepos(username) {
+export async function getPinnedRepos(username) {
   // Validate the input.
   if (!isValidUsername(username)) {
     throw new TypeError('get-pinned-repos expected a valid username');
@@ -29,3 +29,53 @@ module.exports = async function getPinnedRepos(username) {
     .toArray()
     .map((el) => $(el).find('a').attr('href').replace(/^\//, '')); // Remove leading '/' from href.
 };
+
+
+export async function getPinnedRepos_v2(username) {
+  // Validate the input.
+  if (!isValidUsername(username)) {
+    throw new TypeError('get-pinned-repos expected a valid username');
+  }
+
+  // Fetch the user's GitHub page and parse it with Cheerio.
+  const response = await fetch(`https://github.com/${username}`);
+
+  // Check the response status code.
+  if (response.status !== 200) {
+    throw new Error(`Failed to fetch user's GitHub page: ${response.status}`);
+  }
+
+  // Parse the HTML response.
+  const $ = cheerio.load(await response.text());
+  // console.log($.html());
+
+  // Select and extract href attributes of pinned repository links.
+  const pinnedRepos = $('.js-pinned-item-list-item.public')
+    .toArray()
+    .map((el) => $(el).find('a').attr('href').replace(/^\//, '')); // Remove leading '/' from href.
+
+  const lang = $('.js-pinned-item-list-item.public')
+    .toArray()
+    .map((el) => {
+      const desAndLang = $(el).find('p').toArray().map((el) => $(el).text().trim().split(',').map((el) => el.trim()))
+      return desAndLang[1].map(str => str.split('\n')[0])
+    })
+
+  const desc = $('.js-pinned-item-list-item.public')
+    .toArray()
+    .map((el) => {
+      const desAndLang = $(el).find('p').toArray().map((el) => $(el).text().trim().split(',').map((el) => el.trim()))
+      return desAndLang[0]
+    })
+
+  // Return the pinned repos with their descriptions.
+  const pinnedReposWithDescAndLang = pinnedRepos.map((repo, i) => ({
+    repo,
+    description: desc[i].toString(),
+    languages: lang[i].toString()
+  }));
+
+  // console.log(pinnedReposWithDescAndLang);
+  return pinnedReposWithDescAndLang;
+};
+
--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 # get-pinned-repos 
 Get the pinned repos for a github username.
 No token required.
+<br>
+getPinnedRepos_v2 is the recommended version. getPinnedRepos is the old version and will be removed soon.
+<br>
+Feel free to submit feature requests or issues on the github page.
 
 ## Installation
 
@@ -10,16 +14,39 @@ npm install --save @kentaylorappdev/get-pinned-repos
 
 ## Usage
 
-```javascript
-const ghPinnedRepos = require('@kentaylorappdev/get-pinned-repos')
 
-ghPinnedRepos('ktappdev')
+``` javascript
+import { getPinnedRepos_v2 } from '@kentaylorappdev/get-pinned-repos'
+getPinnedRepos_v2('ktappdev')
+  .then(console.log)
+
+[
+  {
+    repo: 'KTAppDev/notes-app-frontend-public',
+    description: 'Secret Note Now you can add or replace an image with every note! Notes and Images stored on "secretnote" can be public or private,depending on the title. Simple titles lead to highly visible notes…',
+    languages: 'Vue'
+  },
+  {
+    repo: 'KTAppDev/tm-nextjs',
+    description: 'Meet Tools Manager: Your NextJS sidekick for keeping all your favorite tools in check. Organize,access,conquer – all with a dash of NextJS magic.',
+    languages: 'TypeScript'
+  },
+... ...
+Array of objects with Repo, Description, and Languages
+  ]
+```
+
+```javascript
+import { getPinnedRepos } from '@kentaylorappdev/get-pinned-repos'
+
+getPinnedRepos('ktappdev')
   .then(console.log)
 
 [ "KTAppDev/notes-app-frontend-public", "KTAppDev/tm-nextjs",
   "KTAppDev/notes-express-api-public", "KTAppDev/Switch-Power-profile",
   "KTAppDev/bad-words-thing", "KTAppDev/review-it-nextjs-v7"
 ]
+This remains in here for anyone who started using the tool from day 1
 ```
 
 ## License
