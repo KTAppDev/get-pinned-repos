9de823c068558dfa6597cb4217027638e0618b06
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-14 06:41:35 -0400

    added v1 and v2 types

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,17 @@
 
 declare module '@kentaylorappdev/get-pinned-repos' {
-  type RepoInfo = {
+  type RepoInfo_v2 = {
     repo: string;
     description: string;
     languages: string;
   };
+type RepoInfo_v1 = {
+    repo: string;
+  };
+  type PinnedRepos_v2 = RepoInfo_v2[];
+  type PinnedRepos_v1 = RepoInfo_v1[];
 
-  type PinnedRepos = RepoInfo[];
-
-  export function getPinnedRepos(username: string): Promise<PinnedRepos>;
+  export function getPinnedRepos_v2(username: string): Promise<PinnedRepos_v2>;
+  export function getPinnedRepos(username: string): Promise<PinnedRepos_v1>;
 }
 
